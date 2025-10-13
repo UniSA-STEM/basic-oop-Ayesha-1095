c09e55745f97a3a323f84b88f1aac09661f8a7c9
Ayesha Siddiqa <siday032@mymail.unisa.edu.au>
2025-10-13 10:35:24 +0300

    Added module docstring to Hacker.py

--- Hacker.py
+++ Hacker.py
@@ -1,8 +1,9 @@
 """
 File: Hacker.py
-Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Description: Defines the Hacker class, representing a cyberpunk hacker who can acquire rigs, manage assets, launch attacks, encrypt/decrypt items, and track trace levels.
+Author: Ayesha Siddiqa
+ID: 110481368
+Username: SIDAY032
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
